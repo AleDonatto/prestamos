52330c3350fe7c8964183b72171f74d8f6cfd19a
aleDonatto <ale_donatto@yahoo.com.mx>
2023-02-13 16:45:18 -0600

    [feat] initial commit and view clientes

---
+++ resources/js/Pages/Clientes.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import { Head } from '@inertiajs/react'
+import Button from '@mui/material/Button';
+
+const Clientes = (props) => {
+    return (
+        <AuthenticatedLayout
+            auth={props.auth}
+            errors={props.errors}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Clientes</h2>}
+        >
+            <Head title="Clientes"></Head>
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <Button variant="contained">Hello World</Button>
+                    </div>
+                </div>
+            </div>
+
+        </AuthenticatedLayout>
+    )
+}
+
+export default Clientes
